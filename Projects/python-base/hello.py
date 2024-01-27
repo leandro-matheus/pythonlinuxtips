3b8bcbddca26e593f85b4f1911ab6dc61cf386bf
Matheus Leandro <mtleandro.oliveira@gmail.com>
2024-01-26 22:07:39 -0300

    Implementamos  o hello world multi linguas

--- Projects/python-base/hello.py
+++ Projects/python-base/hello.py
@@ -1,5 +1,42 @@
 #!/usr/bin/env python3
+"""Hello World Multi Languages.
 
-# Este programa imprime Hello World
-print("Hello,World!")
+Depending on the language configured in the environment
+the program displays the corresponding message.
+
+How to use:
+
+Have the LANG variable properly configured, such as:
+
+    export LANG=pt_BR
+
+
+Execution:
+    python3 hello.py
+    or
+    ./hello.py
+"""
+__version__ = "0.0.1"
+__author__ = "Matheus Leandro"
+__license__ = "Unlicense"
+
+import os
+
+current_language = os.getenv("LANG", "en_US")[:5]
+
+
+msg = "Hello, World!"
+
+
+if current_language == "pt_BR":
+   msg = "Olá, Mundo!"
+elif current_language == "it_IT":
+   msg = "Ciao, Mondo!"
+elif current_language == "es_SP":
+   msg = "Hola Mundo!"
+elif current_language == "fr_FR":
+   msg = "Bonjour, Monde!"
+
+
+print(msg)
 
